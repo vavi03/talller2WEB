82b4f2578d8c2ef55ceb49560432aaf6f52c1f43
vavi03 <vrt0397@gmail.com>
2019-04-10 15:48:47 -0500

    instalar express y creacion handlebars

--- js/tienda.js
+++ js/tienda.js
@@ -1,6 +1,12 @@
 var express= require('express');
+var motorRender = require('express-handlebars');
 var app= express();
+
 
 
 // configurar la carpeta public como "pública"
 app.use(express.static('public'));
+
+
+app.engine('handlebars', motorRender());
+app.set('view engine', 'handlebars');
