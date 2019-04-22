ea2ef1c69cc77bcac4eb09f8f18b5775f5d33e60
vavi03 <vrt0397@gmail.com>
2019-04-21 19:47:59 -0500

    empezar a crear tienda

--- js/tienda.js
+++ js/tienda.js
@@ -10,3 +10,27 @@ app.use(express.static('public'));
 
 app.engine('handlebars', motorRender());
 app.set('view engine', 'handlebars');
+
+
+var productos= [];
+
+productos.push({
+
+    nombre: 'gato',
+    precio: '10000',
+    imagen: 'ggggg',
+    descripcion: 'sdhjsncjks',
+});
+
+
+//configurar ruta inicial
+
+app.get('/', function(req, res){
+res.sendFile(__dirname + '/public/index.html');
+});
+
+
+// iniciar el servidor en el puerto 3000
+app.listen(3000, function() {
+    console.log('Aplicación ejemplo, escuchando el puerto 3000!');
+  });
